defba1ce185219e13465d2f1c468cf50b0af1f1e
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2023-08-26 13:49:15 +0200

    03_busybox added

---
+++ 03_busybox/src/init/init.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    printf("\n");
+    printf("*** mini-linux-distro init ***\n");
+    printf("build with make, musl-libc\n");
+    printf("ENJOY the simplicity!\n");
+    printf("\n");
+    sleep(5);
+    return 0;
+}
